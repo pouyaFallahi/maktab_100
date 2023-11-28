9c071957b470bbfb12e3f1d29b76010c306a0235
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-11-28 10:17:00 +0330

    modified - corrected models.py

--- WEEK22/Task/models.py
+++ WEEK22/Task/models.py
@@ -5,10 +5,15 @@ class Category(models.Model):
     category_name = models.CharField(max_length=150, unique=True)
     category_id = models.IntegerField(unique=True, serialize=True, primary_key=True)
 
+    def __str__(self):
+        return f'{self.category_name}'
+
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
+    def __str__(self):
+        return f'{self.tag_name}'
 
 
 class Task(models.Model):
@@ -16,6 +21,10 @@ class Task(models.Model):
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     task_expire_time = models.TimeField(null=False, blank=False)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    task_title = models.CharField(max_length=100, null=False)
     task_description = models.TextField(null=False, blank=False)
     task_status = models.BooleanField(default=True)
-    tag_id = models.ManyToManyField(Tag, blank=True)
+    tag_id = models.ManyToManyField(Tag, blank=True, related_name='tag')
+
+    def __str__(self):
+        return f'{self.task_title} - {self.task_description[:15]}...'
