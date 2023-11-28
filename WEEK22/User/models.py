56845048bb3c3fe1ada3404d70b0838da33bda4d
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-11-28 10:19:54 +0330

    Modified - corrected User/models.py

--- WEEK22/User/models.py
+++ WEEK22/User/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 import re
-from django.core.exceptions import ValidationError
+from django.core.validators import ValidationError, RegexValidator
 
 
-def phoneNumber_is_valid(phoneNumber=str) -> None:
-    '''regix for check phone number users'''
-    __regex = r'(-2|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}'
-    if not re.match(__regex, phoneNumber):
-        raise ValidationError('phone number not valid')
+# def phoneNumber_is_valid(phoneNumber=str) -> None:
+#     ''' regix for check phone number users '''
+#     __regex = r'^\+98\s?(\d{2})\s?(\d{4})-(\d{4})$'
+#     if not re.match(__regex, phoneNumber):
+#         raise ValidationError('phone number not valid')
 
 
 class Users(models.Model):
@@ -15,7 +15,18 @@ class Users(models.Model):
     user_firsName = models.CharField(max_length=250, null=False, blank=False)
     user_lastName = models.CharField(max_length=250, null=False, blank=False)
     user_email = models.EmailField()
-    user_phoneNumber = models.CharField(max_length=14, validators=[phoneNumber_is_valid])
+    user_phoneNumber = models.CharField(
+        max_length=15,  # Assuming the maximum length for the phone number
+        validators=[
+            RegexValidator(
+                regex=r'^[\+98 | 09 ]\s?(\d{2})\s?(\d{4})(\d{4})$',
+                message='Enter a valid Iranian phone number.'
+            ),
+        ],
+    )
+
+    def __str__(self):
+        return f'{self.user_firsName} - {self.user_lastName}'
 
 
 
